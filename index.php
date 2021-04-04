b1713d341bc7687a63d2d81c48a64e2f22a5df9b
kundan1409 <81857217+kundan1409@users.noreply.github.com>
2021-04-04 12:35:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>NitcB</title>
+  
+<link rel="stylesheet" type="text/css" href="application.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+
+	<script type="text/javascript" src="jquery-3.3.1.js"></script>
+	<script type="text/javascript" src="jquery.cycle.all.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$("#slider").cycle({"fx":"all"});
+		});
+	</script>
+</head>
+
+<body>
+
+ 	
+ 	 <div id="logo">
+ 	 	<img src="logo.png" height="100" align="right=6">
+ 	 </div>	
+ 	 	
+	
+        
+		<b><h1 align="center" >   NATIONAL  INSTITUTE  OF  TECHNOLOGY  CALICUT  </h1> </b>
+<br>
+
+</div>
+ 
+		
+	<div id="footer">
+			<ul>
+
+				<li><a href="index.php">HOME</a></li>
+				<li><a href="about.php">ABOUT</a></li>
+				<li><a href="login.php">LOGIN</a></li>
+				<li><a href="register.php">SIGNUP</a></li>
+				
+			</ul>
+		</div> <!-- menu div -->
+		<div id="content">
+			<div id="slider">
+				<img src="img1.jpg">
+				<img src="img2.jpg">
+				<img src="img3.jpg">
+				<img src="img4.jpg">
+				<img src="img5.jpg">
+			</div>
+		</div> <!-- content div -->
+		<div id="footer">
+					<ul>
+			<li class="category">Social</li>
+		
+			<li><a href="mailto:nidhi_m190366ca@nitc.ac.in">EMAIL</li>
+			<li><a href="https://www.facebook.com/NIT.Calicut.Kerala/">FACEBOOK</li>	
+			<li><a href="https://eduserver.nitc.ac.in/">NITC EDUSERVER</li>	
+			<li><a href="http://www.nitc.ac.in/">WEBSITE</li>
+
+		</ul>
+		
+		</div> <!-- footer div -->
+	</div> <!-- main div -->
+</body>
+</html>
